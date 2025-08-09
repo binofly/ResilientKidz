7ce0bcc7b4089fe14136bc24a50c9c482ddf0cd4
MetaMovie <binofly@gmail.com>
2025-08-09 14:18:45 +0530

    Add files via upload

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "MetaMovie-Resilient Kidz-1.0";
+const contentToCache = [
+    "Build/Mocup02.loader.js",
+    "Build/Mocup02.framework.js.unityweb",
+    "Build/Mocup02.data.unityweb",
+    "Build/Mocup02.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
